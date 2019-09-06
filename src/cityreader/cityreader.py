39dc5ee9ec345871ac6b545e816d04eff29b5a41
bejolo018 <bejolo018@gmail.com>
2019-09-06 11:38:35 -0500

    completed cityreader

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -22,20 +22,28 @@ def __repr__(self):
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+import csv
 cities = []
 
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+    with open('cities.csv') as csv_file:
+      city_csv = csv.reader(csv_file)
+
+      next(city_csv)
+
+      for row in city_csv:
+        cities.append(City(row[0], float(row[3]), float(row[4])))
+
     return cities
 
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c)
+    print(c.name, c.lat, c.lon)
 
 # STRETCH GOAL!
 #
